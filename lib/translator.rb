5916630a9490894946ff47a1bc7478de6965e01e
Elias Taveras <eliastave12@gmail.com>
2020-03-27 02:51:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,13 +3,13 @@ require "yaml"
 require "pry"
 
 def load_library(file_path)
-   library = YAML.load_file(file_path)
-   dict = {"get_meaning" => {}, "get_emoticon" => {}}
-   library.each do |meaning,emoticon|
-    library["get_meaning"][emoticons[1]] = meaning
-    library["get_emoticon"][emoticons[0]] = emoticons[1]
+  library = YAML.load_file(file_path)
+  dict = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    dict["get_meaning"][emoticons[1]] = meaning
+    dict["get_emoticon"][emoticons[0]] = emoticons[1]
   end
-  library
+  dict
 end
 
 def get_japanese_emoticon
